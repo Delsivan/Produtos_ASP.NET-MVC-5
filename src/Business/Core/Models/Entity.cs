8033cc5d35c1a785c3fa417ed49e4f4ea4e34d27
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2023-02-28 11:06:39 -0300

    Adicionando entidades de negócio

---
+++ src/Business/Core/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Business.Core.Models
+{
+    // Uma classe mãe, ou seja uma classe abstrata, ela nao pode ser instanciada ela so pode ser herdada.
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Business/Models/Produtos/Produto.cs
@@ -0,0 +1,26 @@
+﻿using Business.Core.Models;
+using Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models.Produtos
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+
+        /* Entity Framework (EF) Relations 
+         Relacionamento
+        */
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
